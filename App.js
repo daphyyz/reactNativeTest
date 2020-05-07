c3d176cbd5d6648e96ac476b4bcceea6b20816a9
Jeremy Park <icecre4m@gmail.com>
2020-05-07 13:22:23 +0900

    #5 Coding the UI part Three

--- App.js
+++ App.js
@@ -1,101 +1,101 @@
-import React from 'react';
-import {
-  StyleSheet
-  , Text
-  , View
-  , ScrollView
-  , StatusBar
-  , TextInput
-  , Dimensions
-  , Platform
-} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from "react-native";
 import ToDo from "./ToDo";
 
-import { render } from 'react-dom';
+import { render } from "react-dom";
 
-const { height, width } = Dimensions.get("window")
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
-  state = {
-    newToDo: ""
-  };
-  render() {
-    const { newToDo } = this.state;
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <Text style={styles.title}>Kawai To Do</Text>
-        <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} value={newToDo} onChangeText={this._controlNewToDo} placeholderTextColor={"#999"} returnKeyType={"done"} autoCorrect={false} />
-          <ScrollView>
-            <ToDo />
-          </ScrollView>
-        </View>
-      </View>
-    );
-  }
-  _controlNewToDo = text => {
-    this.setState({
-      newToDo: text
-    })
-  }
+    state = {
+        newToDo: "",
+    };
+    render() {
+        const { newToDo } = this.state;
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content" />
+                <Text style={styles.title}>Kawai To Do!!</Text>
+                <View style={styles.card}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder={"New To Do"}
+                        value={newToDo}
+                        onChangeText={this._controlNewToDo}
+                        placeholderTextColor={"#999"}
+                        returnKeyType={"done"}
+                        autoCorrect={false}
+                    />
+                    <ScrollView contentContainerStyle={styles.toDos}>
+                        <ToDo />
+                    </ScrollView>
+                </View>
+            </View>
+        );
+    }
+    _controlNewToDo = (text) => {
+        this.setState({
+            newToDo: text,
+        });
+    };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F23657',
-    alignItems: 'center'
-  },
-  title: {
-    color: "white",
-    fontSize: 30,
-    marginTop: 50,
-    fontWeight: "200",
-    marginBottom: 30
-  },
-  card: {
-    backgroundColor: "white",
-    flex: 1,
-    width: width - 25,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    /**
-     * for iOS 
-     * shadowRadius, shadowOpacity
-     * for Android
-     * elevation
-     */
-    ...Platform.select({
-      ios: {
-        shadowColor: "rgb(50, 50, 50)",
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        shadowOffset: {
-          height: -1,
-          width: 0
-        }
-      },
-      android: {
-        elevation: 5
-      },
-      web: {
-        shadowColor: "rgb(50, 50, 50)",
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        shadowOffset: {
-          height: -1,
-          width: 0
-        }
-      }
-    })
-  },
-  input: {
-    padding: 20,
-    borderBottomColor: "#bbb",
-    borderBottomWidth: 1,
-    fontSize: 25
-
-  }
-
+    container: {
+        flex: 1,
+        backgroundColor: "#F23657",
+        alignItems: "center",
+    },
+    title: {
+        color: "white",
+        fontSize: 30,
+        marginTop: 50,
+        fontWeight: "200",
+        marginBottom: 30,
+    },
+    card: {
+        backgroundColor: "white",
+        flex: 1,
+        width: width - 25,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        /**
+         * for iOS
+         * shadowRadius, shadowOpacity
+         * for Android
+         * elevation
+         */
+        ...Platform.select({
+            ios: {
+                shadowColor: "rgb(50, 50, 50)",
+                shadowOpacity: 0.5,
+                shadowRadius: 5,
+                shadowOffset: {
+                    height: -1,
+                    width: 0,
+                },
+            },
+            android: {
+                elevation: 5,
+            },
+            web: {
+                shadowColor: "rgb(50, 50, 50)",
+                shadowOpacity: 0.5,
+                shadowRadius: 5,
+                shadowOffset: {
+                    height: -1,
+                    width: 0,
+                },
+            },
+        }),
+    },
+    input: {
+        padding: 20,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: 1,
+        fontSize: 25,
+    },
+    toDos: {
+        alignItems: "center",
+    },
 });
